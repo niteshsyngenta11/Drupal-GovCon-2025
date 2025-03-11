26b91f7490a7c32c73e1cdab24779e51ff6f858b
Shayaan <shayaanmomin.sm@gmail.com>
2025-03-11 16:57:01 +0530

    Added Components Documentation

--- web/themes/custom/govcon/components/organisms/announcement-listing/announcement-listing.stories.js
+++ web/themes/custom/govcon/components/organisms/announcement-listing/announcement-listing.stories.js
@@ -1,13 +1,18 @@
-import announcementComponent from './home-page-announcement-demo.twig';
+import announcementComponent from './announcement-listing-demo.twig';
 import data from './announcement-data.yml';
 
 export default {
-  title: "Organisms/Homepage Announcement",
+  title: "Organisms/Announcement Listing",
   argTypes: {
     announcements: {
       control: "object",
       description: "List of announcements to display",
       defaultValue: data.announcements,
+    },
+    allAnnouncement: {
+      control: "object",
+      description: "List of All Announcement",
+      defaultValue: data.viewAnnouncement,
     },
   },
 };
@@ -17,4 +22,5 @@ const Template = (args) => announcementComponent(args);
 export const Default = Template.bind({});
 Default.args = {
   announcements: data.announcements,
+  allAnnouncement: data.viewAnnouncement
 };
