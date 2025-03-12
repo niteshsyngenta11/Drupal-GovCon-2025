6d91596523b3042426d1edbf080b0f917368a35d
Shayaan <shayaanmomin.sm@gmail.com>
2025-03-12 14:18:52 +0530

    Added Variations

--- web/themes/custom/govcon/components/molecules/listing-card/listing-card.stories.js
+++ web/themes/custom/govcon/components/molecules/listing-card/listing-card.stories.js
@@ -18,3 +18,62 @@ const Template = (args) => ListingCard(args);
 
 export const Default = Template.bind({});
 Default.args = data;
+
+export const WithoutImage = Template.bind({});
+WithoutImage.args = {
+  "heading": "Heading",
+  "subheading": "Subheading",
+  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  "cta": {
+    "url": "#",
+    "title": "Button",
+    "target": "_blank",
+    "content": "Button"
+  },
+  "fields": [
+    {
+      "label": "Field One",
+      "value": "Field one content"
+    },
+    {
+      "label": "Field Two",
+      "value": "Field two content"
+    },
+    {
+      "label": "Field Three",
+      "value": "Field three content"
+    },
+    {
+      "label": "Field Four",
+      "value": "Field Four content"
+    }
+  ]
+};
+
+export const WithoutFields = Template.bind({});
+WithoutFields.args = {
+  "image": "https://placehold.co/220x200.png",
+  "image_alt": "Sample image",
+  "heading": "Heading",
+  "subheading": "Subheading",
+  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  "cta": {
+    "url": "#",
+    "title": "Button",
+    "target": "_blank",
+    "content": "Button"
+  },
+};
+
+export const WithoutFieldsImage = Template.bind({});
+WithoutFieldsImage.args = {
+  "heading": "Heading",
+  "subheading": "Subheading",
+  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  "cta": {
+    "url": "#",
+    "title": "Button",
+    "target": "_blank",
+    "content": "Button"
+  },
+};
