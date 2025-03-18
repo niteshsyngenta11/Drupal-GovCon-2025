58f9fa1413c628a777a626f734d0e3ce895fadb2
Shayaan Momin <69762513+shayaanmiyy@users.noreply.github.com>
2025-03-18 14:57:43 +0530

    DGC: 23 - Homepage & Layouting (#64)
    * DGC: 23 - Homepage & Layouting
    
    * Hide the background Image
    
    * Updated Button, Header & Color Changes
    
    * Updated Spacing & Formatted SCSS
    
    * Updated Spacing & Padding
    
    * Removed & from SCSS
    
    * Added line Space

--- web/themes/custom/govcon/components/organisms/header/header.js
+++ web/themes/custom/govcon/components/organisms/header/header.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.querySelector(".hamburger");
   const menu = document.querySelector(".header__menu");
+  const siteHeader = document.querySelector(".site-header");
 
   if (hamburger && menu) {
     hamburger.addEventListener("click", function () {
@@ -18,4 +19,14 @@ document.addEventListener("DOMContentLoaded", function () {
       parentItem.classList.toggle("open");
     });
   });
+
+  if (document.body.classList.contains("has-background-image")) {
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 50) {
+        siteHeader.classList.add("header-color");
+      } else {
+        siteHeader.classList.remove("header-color");
+      }
+    });
+  }
 });
